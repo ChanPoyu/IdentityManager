cb55178dbfbcc8b2553054539b7baca429c2752e
ChanPoyu <dipsy3333@gmail.com>
2019-01-08 23:18:17 +0900

    port changed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const upload = multer(); // for parsing multipart/form-data
 const Joi = require('joi'); 
 const path = require('path');
 const app = express();
-const port = 8000;
+const port = 80;
 const Identity = require('./db/Identity');
 require('dotenv').config();
 
